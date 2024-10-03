94daf44747b11d515e9585c1b1cf8ccb3ca7a4f2
cristina-stroescu <c.stroescu@student.maastrichtuniversity.nl>
2024-10-03 15:14:20 +0200

    changed colors of 3dBalls

--- Project/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAgent.cs
+++ Project/Assets/ML-Agents/Examples/3DBall/Scripts/Ball3DAgent.cs
@@ -7,15 +7,17 @@ using Random = UnityEngine.Random;
 public class Ball3DAgent : Agent
 {
     [Header("Specific to Ball3D")]
-    public GameObject ball;
+    public GameObject ball;  // The single ball
     [Tooltip("Whether to use vector observation. This option should be checked " +
         "in 3DBall scene, and unchecked in Visual3DBall scene. ")]
     public bool useVecObs;
+
     Rigidbody m_BallRb;
     EnvironmentParameters m_ResetParams;
 
     public override void Initialize()
     {
+        // Get the Rigidbody component of the ball
         m_BallRb = ball.GetComponent<Rigidbody>();
         m_ResetParams = Academy.Instance.EnvironmentParameters;
         SetResetParameters();
@@ -25,6 +27,7 @@ public class Ball3DAgent : Agent
     {
         if (useVecObs)
         {
+            // Add observations of the agent's rotation and ball velocity
             sensor.AddObservation(gameObject.transform.rotation.z);
             sensor.AddObservation(gameObject.transform.rotation.x);
             sensor.AddObservation(ball.transform.position - gameObject.transform.position);
@@ -48,6 +51,7 @@ public class Ball3DAgent : Agent
         {
             gameObject.transform.Rotate(new Vector3(1, 0, 0), actionX);
         }
+
         if ((ball.transform.position.y - gameObject.transform.position.y) < -2f ||
             Mathf.Abs(ball.transform.position.x - gameObject.transform.position.x) > 3f ||
             Mathf.Abs(ball.transform.position.z - gameObject.transform.position.z) > 3f)
@@ -63,13 +67,17 @@ public class Ball3DAgent : Agent
 
     public override void OnEpisodeBegin()
     {
+        // Reset agent rotation
         gameObject.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
         gameObject.transform.Rotate(new Vector3(1, 0, 0), Random.Range(-10f, 10f));
         gameObject.transform.Rotate(new Vector3(0, 0, 1), Random.Range(-10f, 10f));
-        m_BallRb.velocity = new Vector3(0f, 0f, 0f);
-        ball.transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 4f, Random.Range(-1.5f, 1.5f))
-            + gameObject.transform.position;
-        //Reset the parameters when the Agent is reset.
+
+        m_BallRb.velocity = Vector3.zero;
+        ball.transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 4f, Random.Range(-1.5f, 1.5f)) + gameObject.transform.position;
+
+        // Change the color of the ball at the beginning of each episode
+        ChangeBallColor();
+
         SetResetParameters();
     }
 
@@ -80,9 +88,16 @@ public class Ball3DAgent : Agent
         continuousActionsOut[1] = Input.GetAxis("Vertical");
     }
 
+    // Function to change the ball's color randomly
+    private void ChangeBallColor()
+    {
+        Renderer ballRenderer = ball.GetComponent<Renderer>();
+        ballRenderer.material.color = new Color(Random.value, Random.value, Random.value);
+    }
+
     public void SetBall()
     {
-        //Set the attributes of the ball by fetching the information from the academy
+        // Set the attributes of the ball by fetching the information from the academy
         m_BallRb.mass = m_ResetParams.GetWithDefault("mass", 1.0f);
         var scale = m_ResetParams.GetWithDefault("scale", 1.0f);
         ball.transform.localScale = new Vector3(scale, scale, scale);
