ba47d413becc986d3a72ff55ee42c5d99a3862a1
Aukje Heijthuijzen <aukje.heijthuijzen@gmail.com>
2024-11-18 22:36:07 +0100

    Added score count for each game

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerBallController.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerBallController.cs
@@ -8,13 +8,30 @@ public class SoccerBallController : MonoBehaviour
     public string purpleGoalTag; //will be used to check if collided with purple goal
     public string blueGoalTag; //will be used to check if collided with blue goal
 
+    public string purpleAgentTag;
+    public string blueAgentTag;
+
+    
+
     void Start()
     {
         envController = area.GetComponent<SoccerEnvController>();
+        Debug.LogWarning($"purpleAgentTag: {purpleAgentTag}");
+        Debug.LogWarning($"blueAgentTag: {blueAgentTag}");
     }
 
     void OnCollisionEnter(Collision col)
     {
+        if (col.gameObject.CompareTag(purpleAgentTag)) // ball touched purple agent 
+        {
+            Debug.LogWarning("Purple player recognized");
+            envController.purplePlayerTouched(Team.Purple);
+        }
+        if (col.gameObject.CompareTag(blueAgentTag)) // ball touched blue agent
+        {
+            Debug.LogWarning("Blue player recognized");
+            envController.bluePlayerTouched(Team.Blue);
+        }
         if (col.gameObject.CompareTag(purpleGoalTag)) //ball touched purple goal
         {
             envController.GoalTouched(Team.Blue);
