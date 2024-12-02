317641eb2bac7a2f2a0c35144a6490cf18cb9917
Chrystalla <c.fella@student.maastrichtuniversity.nl>
2024-12-02 16:56:40 +0100

    sound sensor fully works yay!

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/AgentSoccer.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/AgentSoccer.cs
@@ -12,14 +12,6 @@ public enum Team
 
 public class AgentSoccer : Agent
 {
-    // Note that that the detectable tags are different for the blue and purple teams. The order is
-    // * ball
-    // * own goal
-    // * opposing goal
-    // * wall
-    // * own teammate
-    // * opposing player
-
     public enum Position
     {
         Striker,
@@ -30,7 +22,6 @@ public class AgentSoccer : Agent
     [HideInInspector]
     public Team team;
     float m_KickPower;
-    // The coefficient for the reward for colliding with a ball. Set using curriculum.
     float m_BallTouch;
     public Position position;
 
@@ -39,14 +30,13 @@ public class AgentSoccer : Agent
     float m_LateralSpeed;
     float m_ForwardSpeed;
 
-
     [HideInInspector]
     public Rigidbody agentRb;
     SoccerSettings m_SoccerSettings;
     BehaviorParameters m_BehaviorParameters;
     public Vector3 initialPos;
     public float rotSign;
-    private SoundSensorComponent soundSensorComponent; 
+    private SoundSensorComponent soundSensorComponent;
     EnvironmentParameters m_ResetParams;
 
     public override void Initialize()
@@ -74,6 +64,7 @@ public class AgentSoccer : Agent
             initialPos = new Vector3(transform.position.x + 5f, .5f, transform.position.z);
             rotSign = -1f;
         }
+
         if (position == Position.Goalie)
         {
             m_LateralSpeed = 1.0f;
@@ -89,18 +80,27 @@ public class AgentSoccer : Agent
             m_LateralSpeed = 0.3f;
             m_ForwardSpeed = 1.0f;
         }
+
         m_SoccerSettings = FindObjectOfType<SoccerSettings>();
         agentRb = GetComponent<Rigidbody>();
         agentRb.maxAngularVelocity = 500;
 
         m_ResetParams = Academy.Instance.EnvironmentParameters;
+
+        // Dynamically attach SoundSensorComponent if missing
+        soundSensorComponent = GetComponent<SoundSensorComponent>();
+        if (soundSensorComponent == null)
+        {
+            soundSensorComponent = gameObject.AddComponent<SoundSensorComponent>();
+            Debug.Log("SoundSensorComponent dynamically added to the agent.");
+        }
     }
 
     public void MoveAgent(ActionSegment<int> act)
     {
         var dirToGo = Vector3.zero;
         var rotateDir = Vector3.zero;
-        
+
         m_KickPower = 0f;
 
         var forwardAxis = act[0];
@@ -156,21 +156,17 @@ public class AgentSoccer : Agent
         }
 
         transform.Rotate(rotateDir, Time.deltaTime * 100f);
-        agentRb.AddForce(dirToGo * m_SoccerSettings.agentRunSpeed,ForceMode.VelocityChange);
+        agentRb.AddForce(dirToGo * m_SoccerSettings.agentRunSpeed, ForceMode.VelocityChange);
     }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
-
     {
-
         if (position == Position.Goalie)
         {
-            // Existential bonus for Goalies.
             AddReward(m_Existential);
         }
         else if (position == Position.Striker)
         {
-            // Existential penalty for Strikers
             AddReward(-m_Existential);
         }
         MoveAgent(actionBuffers.DiscreteActions);
@@ -179,45 +175,18 @@ public class AgentSoccer : Agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         var discreteActionsOut = actionsOut.DiscreteActions;
-        //forward
-        if (Input.GetKey(KeyCode.W))
-        {
-            discreteActionsOut[0] = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            discreteActionsOut[0] = 2;
-        }
-        //rotate
-        if (Input.GetKey(KeyCode.A))
-        {
-            discreteActionsOut[2] = 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            discreteActionsOut[2] = 2;
-        }
-        //right
-        if (Input.GetKey(KeyCode.E))
-        {
-            discreteActionsOut[1] = 1;
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            discreteActionsOut[1] = 2;
-        }
+        if (Input.GetKey(KeyCode.W)) discreteActionsOut[0] = 1;
+        if (Input.GetKey(KeyCode.S)) discreteActionsOut[0] = 2;
+        if (Input.GetKey(KeyCode.A)) discreteActionsOut[2] = 1;
+        if (Input.GetKey(KeyCode.D)) discreteActionsOut[2] = 2;
+        if (Input.GetKey(KeyCode.E)) discreteActionsOut[1] = 1;
+        if (Input.GetKey(KeyCode.Q)) discreteActionsOut[1] = 2;
     }
-    /// <summary>
-    /// Used to provide a "kick" to the ball.
-    /// </summary>
+
     void OnCollisionEnter(Collision c)
     {
-        
         var force = k_Power * m_KickPower;
-        if (position == Position.Goalie)
-        {
-            force = k_Power;
-        }
+        if (position == Position.Goalie) force = k_Power;
         if (c.gameObject.CompareTag("ball"))
         {
             AddReward(.2f * m_BallTouch);
@@ -226,30 +195,33 @@ public class AgentSoccer : Agent
             c.gameObject.GetComponent<Rigidbody>().AddForce(dir * force);
         }
     }
-    public override void CollectObservations(VectorSensor sensor)
-        {
-            if (soundSensorComponent != null)
-        {
-            // Get sound data from the SoundSensorComponent
-            float[] soundData = soundSensorComponent.GetSensorData();
-            Debug.Log($"SoundSensorComponent Observations: {string.Join(", ", soundData)}");
 
-            // Add sound data as observations
-            foreach (var data in soundData)
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        if (soundSensorComponent != null)
+        {
+            float[] soundData = soundSensorComponent.GetSensorData();
+            if (soundData != null && soundData.Length > 0)
             {
-                sensor.AddObservation(data);
+                Debug.Log($"SoundSensorComponent Observations: {string.Join(", ", soundData)}");
+                foreach (var data in soundData)
+                {
+                    sensor.AddObservation(data);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("SoundSensorComponent returned no sound data.");
             }
         }
         else
         {
             Debug.LogWarning("SoundSensorComponent is not attached to the agent.");
         }
-        }
+    }
 
-    
     public override void OnEpisodeBegin()
     {
         m_BallTouch = m_ResetParams.GetWithDefault("ball_touch", 0);
     }
-
 }
